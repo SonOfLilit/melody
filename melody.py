c6f47df601d67e149cf9397fa06f5d4767a987f2
Aur Saraf <aur@loris.co.il>
2015-01-01 17:26:19 +0200

    play many notes at once, repeat()

--- melody.py
+++ melody.py
@@ -1,4 +1,5 @@
 import midi
+from heapq import heappush, heappop
 
 for name in 'pitch time duration'.split():
     globals()[name.upper()] = name
@@ -47,6 +48,16 @@ except ValueError:
 else:
     assert False
 
+def delay(delta, notes):
+    for note in notes:
+        note = dict(note)
+        note[TIME] += delta
+        yield note
+assert list(delay(100, [])) == []
+assert list(delay(100, [{TIME: 5}])) == [{TIME: 105}]
+assert list(delay(100, [{TIME: 5}, {TIME: 15}])) == [{TIME: 105}, {TIME: 115}]
+assert list(delay(0, [{TIME: 5}, {TIME: 15}])) == [{TIME: 5}, {TIME: 15}]
+
 def merge(melodies):
     melodies = map(iter, melodies)
     firsts = []
@@ -94,18 +105,103 @@ except ValueError:
 else:
     assert False
 
+class StreamCloner(object):
+    def __init__(self, stream):
+        self.stream = stream
+        self.past = []
+
+    def read(self):
+        """
+        Appends next item to self.past, raises StopIteration if no more items.
+        """
+        self.past.append(self.stream.next())
+
+    def clone(self):
+        i = 0
+        while True:
+            while i >= len(self.past):
+                self.read()
+            yield self.past[i]
+            i += 1
+stream = StreamCloner(iter([1, 2, 3]))
+assert list(stream.clone()) == list(stream.clone()) == [1, 2, 3]
+stream = StreamCloner(iter([1, 2, 3]))
+assert zip(stream.clone(), stream.clone()) == [(1, 1), (2, 2), (3, 3)]
+
+def repeat(n, period, notes):
+    cloner = StreamCloner(iter(notes))
+    clones = (cloner.clone() for i in xrange(n))
+    return merge(delay(i * period, notes_tee) for i, notes_tee in enumerate(clones))
+assert list(repeat(3, 100, [])) == []
+assert list(repeat(3, 100, [{TIME: 0, DURATION: 100}])) == [
+    {TIME: 0, DURATION: 100},
+    {TIME: 100, DURATION: 100},
+    {TIME: 200, DURATION: 100},
+]
+assert list(repeat(3, 100, [{TIME: 0, DURATION: 50}])) == [
+    {TIME: 0, DURATION: 50},
+    {TIME: 100, DURATION: 50},
+    {TIME: 200, DURATION: 50},
+]
+assert list(repeat(3, 75, [{TIME: 0, DURATION: 50}, {TIME: 100, DURATION: 100}])) == [
+    {TIME: 0, DURATION: 50},
+    {TIME: 75, DURATION: 50},
+                             {TIME: 100, DURATION: 100},
+    {TIME: 150, DURATION: 50},
+                             {TIME: 175, DURATION: 100},
+                             {TIME: 250, DURATION: 100},
+]
+m = lambda: melody([
+            {'duration': 100, 'pitch': midi.C_4},
+            {'duration': 100, 'pitch': midi.E_4}])
+print list(m())
+print list(repeat(1, 100, m()))
+print list(repeat(2, 100, m()))
+assert len(list(repeat(2, 100, m()))) == 2 * 2
+assert len(list(repeat(4, 100, m()))) == 4 * 2
+
 def to_ticks(duration):
     return duration
 
+def counter(i=0):
+    while True:
+        yield i
+        i += 1
+
 def to_events(notes):
     time = 0
+    queue = []
+    # makes the queue sorting stable
+    count = counter()
     for note in notes:
-        assert note[TIME] >= time
+        note_time = to_ticks(note[TIME])
         duration = to_ticks(note[DURATION])
-        yield midi.NoteOnEvent(tick=note[TIME] - time, pitch=note[PITCH], velocity=100)
-        time = note[TIME]
-        yield midi.NoteOffEvent(tick=duration, pitch=note[PITCH])
-        time += duration
+        heappush(queue, (note_time, count.next(), (midi.NoteOnEvent, note[PITCH])))
+        heappush(queue, (note_time + duration, count.next(), (midi.NoteOffEvent, note[PITCH])))
+        while True:
+            if not queue:
+                # no more pending events, next note!
+                break
+            event_data = heappop(queue)
+            event_time, _, (event_type, pitch) = event_data
+            if event_time > note_time:
+                # we can't know which is the next event
+                # until we see when the next note starts
+                # (until now we could because we knew the
+                # next note starts at note_time)
+
+                # put it back
+                heappush(queue, event_data)
+                break
+            assert event_time >= time
+            yield event_type(tick=event_time - time, pitch=pitch, velocity=100)
+            time = event_time
+    # drain all remaining events
+    while queue:
+        event_time, _, (event_type, pitch) = heappop(queue)
+        assert event_time >= time
+        yield event_type(tick=event_time - time)
+        time = event_time
 assert list(to_events([])) == []
 assert list(to_events([
     {TIME: 0, DURATION: 100, PITCH: midi.C_3},
@@ -114,9 +210,6 @@ assert list(to_events([
         midi.NoteOffEvent(tick=100, pitch=midi.C_3, velocity=100),
         midi.NoteOnEvent(tick=0, pitch=midi.D_3, velocity=100),
         midi.NoteOffEvent(tick=100, pitch=midi.D_3, velocity=100)]
-print list(to_events([
-    {TIME: 0, DURATION: 50, PITCH: midi.C_3},
-    {TIME: 100, DURATION: 100, PITCH: midi.D_3}]))
 assert list(to_events([
     {TIME: 0, DURATION: 50, PITCH: midi.C_3},
     {TIME: 100, DURATION: 100, PITCH: midi.D_3}])) == [
@@ -124,6 +217,13 @@ assert list(to_events([
         midi.NoteOffEvent(tick=50, pitch=midi.C_3, velocity=100),
         midi.NoteOnEvent(tick=50, pitch=midi.D_3, velocity=100),
         midi.NoteOffEvent(tick=100, pitch=midi.D_3, velocity=100)]
+assert list(to_events([
+    {TIME: 0, DURATION: 400, PITCH: midi.C_3},
+    {TIME: 100, DURATION: 100, PITCH: midi.D_3}])) == [
+        midi.NoteOnEvent(tick=0, pitch=midi.C_3, velocity=100),
+        midi.NoteOnEvent(tick=100, pitch=midi.D_3, velocity=100),
+        midi.NoteOffEvent(tick=100, pitch=midi.D_3, velocity=100),
+        midi.NoteOffEvent(tick=200, pitch=midi.C_3, velocity=100)]
 
 def to_midi(notes, tempo=140):
     pattern = midi.Pattern()
--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 import melody
 import midi
 
-arpeggio = list(
-    melody.melody([
-        {'duration': 100, 'pitch': midi.C_4},
-        {'duration': 100, 'pitch': midi.E_4},
-        {'duration': 100, 'pitch': midi.G_4},
-        {'duration': 100, 'pitch': midi.C_6},
-        {'duration': 100, 'pitch': midi.G_4},
-        {'duration': 100, 'pitch': midi.E_4},
-    ])) + [{'time': 700, 'duration': 400, 'pitch': midi.C_4}]
-midi = melody.to_midi(arpeggio)
-print midi
-melody.write(midi, 'test.mid')
+arpeggio = melody.repeat(4, 600,
+    melody.merge([
+        melody.melody([
+            {'duration': 600, 'pitch': midi.C_3}
+        ]), melody.melody([
+            {'duration': 100, 'pitch': midi.C_4},
+            {'duration': 100, 'pitch': midi.E_4},
+            {'duration': 100, 'pitch': midi.G_4},
+            {'duration': 100, 'pitch': midi.C_6},
+            {'duration': 100, 'pitch': midi.G_4},
+            {'duration': 100, 'pitch': midi.E_4},
+        ])
+    ])
+)
+pattern = melody.to_midi(arpeggio)
+#print pattern
+melody.write(pattern, 'test.mid')
